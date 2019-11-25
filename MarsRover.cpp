1c1934eb44c6e5b886c159d58eec9a87c9b937a4
Deep knowledge <alrazqii@gmail.com>
2019-11-25 13:47:06 +0100

    d

--- MarsRover.cpp
+++ MarsRover.cpp
@@ -113,18 +113,3 @@ void MarsRover ::move_Forward()
 MarsRover::~MarsRover(){
         
 }
-
- int main(){
-
- MarsRover Rover1;
- Rover1.Set_Position(1,2,'N');
- Rover1.Set_Instructions("LMLMLMLMM");
- Rover1.show_CurrentPosition();
-
- Rover1.Set_Position(3,3,'E');
-Rover1.Set_Instructions("MMRMMRMRRM");
- Rover1.show_CurrentPosition();
-
- return 0;
- }
-
